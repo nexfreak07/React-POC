cf3ffb2fa095da7630409d95ea51fc70b8304f54
Akash Dasgupta <akash.dasgupta@cyberproof.com>
2023-06-07 03:10:02 +0530

    UI Created

---
+++ user-list/src/components/Users/AddUser.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+import Button from "../UI/Button";
+import { useState } from "react";
+
+const Adduser = () => {
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(""); 
+
+    const nameHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const ageHandler = (event) => {
+        setAge(event.target.value);
+    }
+
+    const addUserHandler = (event) => {
+        event.preventDefault();
+
+        if(name.trim().length === 0 || age.trim().length === 0 )
+        {
+            return;
+        }
+        if(+age < 1) // + is to convert a string to a number, as JSX fetches strings
+        {
+            return;
+        }
+        const newUser = {
+            Name : name,
+            Age : +age
+        }
+
+        setName("");
+        setAge("");
+
+    }
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input id="username" type="text" onChange={nameHandler} value={name}></input>
+        <label htmlFor="username">Age (in Years)</label>
+        <input id="age" type="number" onChange={ageHandler} value={age}></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Adduser;
